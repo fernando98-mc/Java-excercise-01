148988b80f35f60de8913de1eeab2bded806303e
fernando98-mc <luisf543678@gmail.com>
2023-10-18 20:19:17 -0600

    add test-1 Scanner

---
+++ src/Prueba_1/Test1.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Prueba_1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Userr
+ */
+public class Test1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner name = new Scanner(System.in);
+        String inputName = name.next();
+        System.out.println("Hola ..." .concat(inputName));
+    }
+    
+}
